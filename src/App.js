bed97e796e45a67827c2d953cbe6f8350d6ff69e
Carolyn Selheim <carolyn.selheim@gmail.com>
2018-02-02 16:08:19 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Header from "./Header";
-import About from "./About";
-import Footer from ".Footer";
+import About from "./about";
+import Iframe from "./Reader/Iframe";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
         <Router>
-          <Route exact path="/" component={About} />
-          <Route path="/about" component={About} />
-          <Route path="/get-started" />
-          <Route path="/reader" />
+          <div>
+            <Route exact path="/" component={About} />
+            <Route path="/about" component={About} />
+            <Route path="/get-started" />
+            <Route path="/reader" component={Iframe} />
+          </div>
         </Router>
-        <Footer />
       </div>
     );
   }
