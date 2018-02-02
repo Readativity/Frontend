bb90d6339f173cfe4cdd20bbf519de41f065ff48
Carolyn Selheim <carolyn.selheim@gmail.com>
2018-02-02 15:51:10 -0700

    added router to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Header from "./Header";
+import About from "./About";
+import Footer from ".Footer";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+          <Router>
+            <Route exact path="/" component={About}/>
+            <Route path="/about" component={About}/>
+            <Route path="/get-started">
+            <Route path="/reader"/>
+          </Router>
+        <Footer />
       </div>
     );
   }
