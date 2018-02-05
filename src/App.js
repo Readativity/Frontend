9bf9e98f7199eb4ebe7def87ea89c5790f452c23
Carolyn Selheim <carolyn.selheim@gmail.com>
2018-02-05 13:22:56 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import About from "./about";
-import Iframe from "./Reader/Iframe";
+import Reader from "./Reader";
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -17,7 +17,7 @@ class App extends Component {
             <Route exact path="/" component={About} />
             <Route path="/about" component={About} />
             <Route path="/get-started" />
-            <Route path="/reader" component={Iframe} />
+            <Route path="/reader" component={Reader} />
           </div>
         </Router>
         <Footer />
