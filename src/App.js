455ca80657c9d6b6bbfff82a4afe3a690a223a52
kmkingdon <32685092+kmkingdon@users.noreply.github.com>
2018-02-05 14:30:24 -0700

    added modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import About from "./about";
-import Iframe from "./Reader/Iframe";
+import Reader from "./Reader";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      users: [],
+      userId: '',
+      modalIsOpen: false
     };
+    this.handleLogin = this.handleLogin.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
   componentDidMount() {
@@ -24,17 +28,37 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  handleLogin(e) {
+    e.preventDefault();
+    let username = e.target[0].value;
+    let password = e.target[1].value;
+
+
+    this.state.users.forEach(user => {
+      if(user.username === username && user.password === password) {
+        this.setState({userId:user.id})
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <Router>
           <div>
-            <Route exact path="/" component={About} />
+            <Route exact path="/" component={Splash openModal={this.openModal} closeModal={this.closeModal} handleLogin={this.handleLogin} } />
             <Route path="/about" component={About} />
             <Route path="/get-started" />
             <Route
               path="/reader"
-              component={() => <Iframe article={this.state.article} />}
+              component={() => <Reader userId={this.state.userId} />}
             />
           </div>
         </Router>
@@ -44,34 +68,3 @@ class App extends Component {
 }
 
 export default App;
-=======
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import About from "./about";
-import Reader from "./Reader";
-import Header from "./Header";
-import Footer from "./Footer";
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Router>
-          <div>
-            <Route exact path="/" component={About} />
-            <Route path="/about" component={About} />
-            <Route path="/get-started" />
-            <Route path="/reader" component={Reader} />
-          </div>
-        </Router>
-        <Footer />
-      </div>
-    );
-  }
-}
-
-export default App;
->>>>>>> 76d9967f822234507c24b961d25e15fcde470f89
---
+++ src/login/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Link} from 'react-router-dom'
+import Modal from 'react-modal';
+import styled from "styled-components";
+
+class Login extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  const LoginForm =styled.div `
+      display: grid;
+      grid-template-columns: 100%;
+      grid-template-rows: 10% 10% 80%
+  `
+  const Close = styled.img `
+      grid-row: 1/2;
+      grid-column: 1/2;
+      justify-self: end;
+      align-self: start;
+  `
+  const Header = styled.h1 `
+      grid-row: 2/3;
+      grid-column: 1/2;
+      justify-self: center;
+      align-self: center;
+      font-size: 1.5rem;
+  `
+  const FormLogin = styled.form `
+      grid-row: 3/4;
+      grid-column: 1/2;
+      display: flex;
+      flex-flow: column;
+      justify-items: center;
+      align-items: center;
+  `
+
+  render() {
+    return (
+      <Modal
+        isOpen={this.props.modalIsOpen}
+        onRequestClose={this.props.closeModal}
+        className={{
+          base: 'modal'
+        }}
+        contentLabel="Login"
+        >
+          <div>
+            <LoginForm>
+              <Close src="./assets/close.png" alt="close" onClick={this.props.closeModal}/>
+              <Header>Login</Header>
+              <FormLogin onSubmit={this.props.handleLogin}>
+                <label htmlFor="username"> Username</label>
+                <input name="username" placeholder="Demouser" />
+                <label htmlFor="password"> Password</label>
+                <input name="password" placeholder="1234"/>
+                <input type="submit" name="submit" defaultValue="Sign In" />
+              </FormLogin>
+            </LoginForm>
+          </div>
+          <div>
+
+          </div>
+        </Modal>
+    )
+  }
+}
+
+export default StudentModal;
