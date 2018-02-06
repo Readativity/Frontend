da30464220cedca7e44f2e039cea0c2d39b8ed6d
kmkingdon <32685092+kmkingdon@users.noreply.github.com>
2018-02-06 14:29:51 -0700

    updating to master

--- src/about/index.js
+++ src/about/index.js
@@ -3,53 +3,51 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const About = props => {
-  const AboutDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  `;
 
-  const Title = styled.h1`
-    font-size: 2rem;
-    color: grey;
-  `;
-  const InfoContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  `;
-  const Subtitle = styled.h2`
-    font-size: 1.5rem;
-  `;
-  const Information = styled.p`
-    margin-bottom: 1rem;
-    font-size: 1rem;
-    width: 50%;
-    text-align: center;
-  `;
-
-  const StyledLink = styled(Link)`
-    text-decoration: none;
-    padding: 1rem;
-    border: black solid 1px;
-    background-color: red;
-    color: white;
-  `;
 
   return (
-    <div className="about">
-      <Title> Pocket Pooper </Title>
+    <div className="main">
+      <Title> Readativity </Title>
       <InfoContainer>
         <Subtitle> What is Readativity?</Subtitle>
         <Information>Readativity encourages you to make time </Information>
         <Subtitle> Why do you need Readativity?</Subtitle>
-        <Information />
+        <Information>Readativity encourages you to make time </Information>
+        <Subtitle> Our Team:</Subtitle>
+        <Information>Bj Collins, Carolyn Selheim, James Mann, and Kevin Kingdon</Information>
+        <Subtitle> Our Code:</Subtitle>
+        <Information><a href="https://github.com/Readativity/Frontend" target="_blank">View Our Code Here</a></Information>
       </InfoContainer>
-      <StyledLink to="/reader">Start Reading</StyledLink>
     </div>
   );
 };
 
 export default About;
+
+const AboutDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  margin-top: 1rem;
+`;
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Subtitle = styled.h2`
+  font-size: 1.5rem;
+  margin-top: 1rem;
+`;
+const Information = styled.p`
+  margin-top: 1rem;
+  font-size: 1rem;
+  width: 50%;
+  text-align: center;
+`;
