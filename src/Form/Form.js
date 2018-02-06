91f1554754da78a7cc8981ddfd1c3e2614a0a60b
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-02-06 10:31:04 -0700

    m

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -21,43 +21,43 @@ function Form() {
         <ArticleCatagories>
 
           <label HTMLfor="general" >
-            <input type="checkbox" name="general" value="valuable" id="general" />
+            <input type="checkbox" name="general" value="general" id="general" />
             <img src="http://funwithbonus.com/wp-content/uploads/npr_news_icon.png" alt="some img" />
             <h3>GENERAL</h3>
           </label>
 
           <label HTMLfor="Technology" >
-            <input type="checkbox" name="Technology" value="valuable" id="Technology" />
+            <input type="checkbox" name="Technology" value="technology" id="Technology" />
             <img src="http://thumbs.microstock.in/unsafe/http://microstock-in.s3.amazonaws.com/catalog/product/thumbnail/2/BUSINESS-AND-FINANCE/Business_10068862_thumbnail.jpg" alt="some img" />
             <h3>TECHNOLOGY</h3>
           </label>
 
           <label HTMLfor="Health" >
-            <input type="checkbox" name="Health" value="valuable" id="Health" />
+            <input type="checkbox" name="Health" value="health" id="Health" />
             <img src="https://cdn.kastatic.org/genfiles/topic-icons/icons/health.png-0b07ae-128c.png" alt="some img" />
             <h3>HEALTH</h3>
           </label>
 
           <label HTMLfor="Entertainment" >
-            <input type="checkbox" name="Entertainment" value="valuable" id="Entertainment" />
+            <input type="checkbox" name="Entertainment" value="entertainment" id="Entertainment" />
             <img src="http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons-256/yellow-road-sign-icons-sports-hobbies/048802-yellow-road-sign-icon-sports-hobbies-masks-sc37.png" alt="some img" />
             <h3>ENTERTAINMENT</h3>
           </label>
 
           <label HTMLfor="Business" >
-            <input type="checkbox" name="Business" value="valuable" id="Business" />
+            <input type="checkbox" name="Business" value="business" id="Business" />
             <img src="https://i.pinimg.com/736x/42/96/22/429622a0f8d1f1e45d6d0d59d25d5c14--job-posting-job-search.jpg" alt="some img" />
             <h3>BUSINESS</h3>
           </label>
 
           <label HTMLfor="Sports" >
-            <input type="checkbox" name="Sports" value="valuable" id="Sports" />
+            <input type="checkbox" name="Sports" value="sports" id="Sports" />
             <img src="http://www.root6technology.com/wp-content/uploads/2014/08/BBC_Sport.jpg" alt="some img" />
             <h3>SPORTS</h3>
           </label>
 
           <label HTMLfor="Science" >
-            <input type="checkbox" name="Science" value="valuable" id="Science" />
+            <input type="checkbox" name="Science" value="science" id="Science" />
             <img src="https://pi.tedcdn.com/r/pf.tedcdn.com/images/playlists/how_were_fighting_cancer_1107183452.jpg?quality=89&w=256" alt="some img" />
             <h3>SCIENCE</h3>
           </label>
